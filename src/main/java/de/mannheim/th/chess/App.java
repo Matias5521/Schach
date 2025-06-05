9c0b827e97ecea8d0d52f0f5ce52f98ee138e5cb
Matias <3020772@tud.hs-mannheim.de>
2025-06-05 19:48:47 +0200

    .

--- src/main/java/de/mannheim/th/chess/App.java
+++ src/main/java/de/mannheim/th/chess/App.java
@@ -1,9 +1,12 @@
 package de.mannheim.th.chess;
 
 /**
- * Hello world!
+ * Eine einfache Schach App mithilfe von {@linkplain https://github.com/bhlangonijr/chesslib}
+ * @author Matias Mas Viehl, Dominik Stuck und Marius Guendel
+ * @version 0.0.1
  */
 public class App {
+	
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
