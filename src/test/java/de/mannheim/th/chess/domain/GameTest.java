7fcf514d7a0d1d9aaf27a1a58f651c4b427d91b6
dstuck <stuck.dominik@gmail.com>
2025-06-18 04:41:53 +0200

    add tests

--- src/test/java/de/mannheim/th/chess/domain/GameTest.java
+++ src/test/java/de/mannheim/th/chess/domain/GameTest.java
@@ -2,6 +2,7 @@ package de.mannheim.th.chess.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -23,5 +24,32 @@ public class GameTest {
     assertEquals("a2a3", list.getFirst().toString());
     assertEquals("a2a4", list.getLast().toString());
 
+  }
+
+  @Test
+  void getLegalMoveableSquaresTest() {
+    Game game = new Game();
+    Square square = Square.A2;
+
+    List<Square> controllList = Arrays.asList(Square.A3, Square.A4);
+    assertEquals(controllList, game.getLegalMoveableSquares(square));
+
+    game = new Game("k7/8/8/8/8/8/8/K6N w - - 0 1");
+    square = Square.H1;
+    controllList = Arrays.asList(Square.F2, Square.G3);
+    assertEquals(controllList, game.getLegalMoveableSquares(square));
+  }
+
+  @Test
+  void getAllLegalMoveableSquaresTest() {
+    Game game = new Game();
+
+    List<Square> controllList = Arrays.asList(Square.A2, Square.B2, Square.C2, Square.D2, Square.E2, Square.F2,
+        Square.G2, Square.H2, Square.B1, Square.G1);
+    assertEquals(controllList, game.getAllLegalMoveableSquares());
+
+    game = new Game("k7/8/8/8/8/8/8/K6N w - - 0 1");
+    controllList = Arrays.asList(Square.H1, Square.A1);
+    assertEquals(controllList, game.getAllLegalMoveableSquares());
   }
 }
